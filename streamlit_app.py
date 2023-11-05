c8133f317e21bab5536ee6d9f47e16518691ba9d
hckyou7 <128127175+hckyou7@users.noreply.github.com>
2023-11-05 13:17:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ import numpy as np
 
 
 st.title('Uber pickups in NYC')
+
+
+import matplotlib.pyplot as plt
+# Title for the Streamlit app
+st.title("Random Data Histogram")
+# Generate random data
+data = np.random.randn(1000)
+# User input for number of bins using Streamlit's slider
+bins = st.slider("Choose number of bins for histogram", 10, 100, 50)
+# Plot histogram
+fig, ax = plt.subplots()
+ax.hist(data, bins=bins)
+ax.set_title("Random Data Histogram")
+ax.set_xlabel("Value")
+ax.set_ylabel("Frequency")
+st.pyplot(fig)
